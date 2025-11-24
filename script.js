1047deb84749a3f663a8c647efcff12de4fca79e
Ujjawal Shakya <98425350+ujjawal10024@users.noreply.github.com>
2025-11-24 13:38:25 +0530

    Initial Bugatti website upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Page Loaded");
+
+    const  heading = document.querySelector(".hero-content h1");
+    heading.style.color = "#ff003c";
+});
+
+console.log(hello)
